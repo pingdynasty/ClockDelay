44f26d12728f20f63af10c600ff2f9d4b07edc06
mars <martin@rebel-it.com>
2012-05-08 01:29:13 +0100

    additional files, wiring serial implementation

---
+++ device.h
@@ -0,0 +1,48 @@
+/*
+  pin mappings
+ */
+
+#define DIVIDE_ADC_CHANNEL              0
+#define DELAY_ADC_CHANNEL               1
+
+#define MODE_SWITCH_DDR                 DDRB
+#define MODE_SWITCH_PINS                PINB
+#define MODE_SWITCH_PORT                PORTB
+#define MODE_SWITCH_PIN_A               PORTB4
+#define MODE_SWITCH_PIN_B               PORTB5
+
+#define CLOCKDELAY_CLOCK_DDR            DDRD
+#define CLOCKDELAY_CLOCK_PORT           PORTD
+#define CLOCKDELAY_CLOCK_PINS           PIND
+#define CLOCKDELAY_CLOCK_PIN            PORTD3
+
+#define CLOCKDELAY_RESET_DDR            DDRD
+#define CLOCKDELAY_RESET_PORT           PORTD
+#define CLOCKDELAY_RESET_PINS           PIND
+#define CLOCKDELAY_RESET_PIN            PORTD2
+
+#define DIVIDE_OUTPUT_DDR               DDRB
+#define DIVIDE_OUTPUT_PINS              PINB
+#define DIVIDE_OUTPUT_PORT              PORTB
+#define DIVIDE_OUTPUT_PIN_A             PORTB0
+#define DIVIDE_OUTPUT_PIN_B             PORTB4
+
+#define DELAY_OUTPUT_DDR                DDRB
+#define DELAY_OUTPUT_PINS               PINB
+#define DELAY_OUTPUT_PORT               PORTB
+#define DELAY_OUTPUT_PIN_A              PORTB1
+#define DELAY_OUTPUT_PIN_B              PORTB3
+
+#define COMBINED_OUTPUT_DDR             DDRB
+#define COMBINED_OUTPUT_PINS            PINB
+#define COMBINED_OUTPUT_PORT            PORTB
+#define COMBINED_OUTPUT_PIN             PORTB5
+
+#define CLOCKDELAY_LEDS_DDR        DDRB
+#define CLOCKDELAY_LEDS_PORT       PORTB
+#define CLOCKDELAY_LEDS_PINS       PINB
+#define CLOCKDELAY_LED_A_PIN       PORTB4
+#define CLOCKDELAY_LED_B_PIN       PORTB3
+#define CLOCKDELAY_LED_C_PIN       PORTB5
+
+
